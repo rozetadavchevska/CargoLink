b9733e06fa78fa5f2a64dbf6b18515b308cb293c
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-12-11 23:27:52 +0100

    added design to the fragments

--- app/src/main/java/com/ap/cargolink/ui/fragments/DriverHomeFragment.java
+++ app/src/main/java/com/ap/cargolink/ui/fragments/DriverHomeFragment.java
@@ -2,20 +2,52 @@ package com.ap.cargolink.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ap.cargolink.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DriverHomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_driver_home, container, false);
+
+        TextView userName = view.findViewById(R.id.userName);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        String currentUserId = currentUser.getUid();
+
+        getUserName(currentUserId, userName);
+
+        return view;
+    }
+
+    private void getUserName(String currentUserId, TextView userName) {
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users").child(currentUserId);
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String firstName = snapshot.child("firstName").getValue(String.class);
+                    userName.setText(firstName);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
     }
 }
