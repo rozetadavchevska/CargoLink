7994e591784d343025d4976eded51a45b1935fa1
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-12-28 17:34:41 +0100

    added functionality for driver to confirm delivery

---
+++ app/src/main/java/com/ap/cargolink/data/adapters/DriverOrdersAdapter.java
@@ -0,0 +1,128 @@
+package com.ap.cargolink.data.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ap.cargolink.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class DriverOrdersAdapter extends RecyclerView.Adapter<DriverOrdersAdapter.ViewHolder> {
+    private List<String> ordersList;
+    private FragmentManager fragmentManager;
+
+    public DriverOrdersAdapter(List<String> ordersList, FragmentManager fragmentManager){
+        this.ordersList = ordersList;
+        this.fragmentManager = fragmentManager;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_driver_order, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String orderId = ordersList.get(position);
+
+        getOrderDetails(orderId, holder);
+
+        checkOrderConfirmationStatus(orderId, holder);
+
+        holder.confirmDeliveredBtn.setOnClickListener(v -> confirmDelivered(orderId, "Delivered"));
+    }
+
+    private void confirmDelivered(String orderId, String status) {
+        DatabaseReference ordersRef = FirebaseDatabase.getInstance().getReference("orders").child(orderId);
+        ordersRef.child("orderStatus").setValue(status);
+        notifyDataSetChanged();
+    }
+
+    private void checkOrderConfirmationStatus(String orderId, ViewHolder holder) {
+        DatabaseReference ordersRef = FirebaseDatabase.getInstance().getReference("orders").child(orderId);
+        ordersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String orderStatus = snapshot.child("orderStatus").getValue(String.class);
+
+                    if ("Delivered".equals(orderStatus)) {
+                        holder.confirmDeliveredBtn.setText("Confirmed");
+                        holder.confirmDeliveredBtn.setEnabled(false);
+                        notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void getOrderDetails(String orderId, ViewHolder holder) {
+        DatabaseReference ordersRef = FirebaseDatabase.getInstance().getReference("orders").child(orderId);
+        ordersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String name = snapshot.child("orderName").getValue(String.class);
+                    String description = snapshot.child("orderDescription").getValue(String.class);
+                    String addressFrom = snapshot.child("addressFrom").getValue(String.class);
+                    String addressTo = snapshot.child("addressTo").getValue(String.class);
+                    Long weight = snapshot.child("orderWeight").getValue(Long.class);
+                    String receiver = snapshot.child("orderReceiver").getValue(String.class);
+                    String receiverNumber = snapshot.child("receiverNumber").getValue(String.class);
+
+
+                    holder.orderName.setText(name);
+                    holder.orderDescriptionText.setText(description);
+                    holder.orderFromAddressText.setText(addressFrom);
+                    holder.orderToAddressText.setText(addressTo);
+                    holder.orderWeightText.setText(String.valueOf(weight));
+                    holder.orderReceiverText.setText(receiver);
+                    holder.orderPhoneText.setText(receiverNumber);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return ordersList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView orderName, orderDescriptionText, orderFromAddressText, orderToAddressText, orderWeightText, orderReceiverText, orderPhoneText;
+        Button confirmDeliveredBtn;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            orderName = itemView.findViewById(R.id.orderName);
+            orderDescriptionText = itemView.findViewById(R.id.orderDescriptionText);
+            orderFromAddressText = itemView.findViewById(R.id.orderFromAddressText);
+            orderToAddressText = itemView.findViewById(R.id.orderToAddressText);
+            orderWeightText = itemView.findViewById(R.id.orderWeightText);
+            orderReceiverText = itemView.findViewById(R.id.orderReceiverText);
+            orderPhoneText = itemView.findViewById(R.id.orderPhoneText);
+            confirmDeliveredBtn = itemView.findViewById(R.id.confirmDeliveredBtn);
+        }
+    }
+}
+
--- app/src/main/java/com/ap/cargolink/ui/fragments/DriverOrdersFragment.java
+++ app/src/main/java/com/ap/cargolink/ui/fragments/DriverOrdersFragment.java
@@ -2,20 +2,67 @@ package com.ap.cargolink.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ap.cargolink.R;
+import com.ap.cargolink.data.adapters.DriverOrdersAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DriverOrdersFragment extends Fragment {
+    private List<String> ordersList;
+    private DriverOrdersAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_orders, container, false);
+        View view = inflater.inflate(R.layout.fragment_driver_orders, container, false);
+
+        RecyclerView orders = view.findViewById(R.id.ordersList);
+        FragmentManager fragmentManager = getParentFragmentManager();
+        ordersList = new ArrayList<>();
+        getOrders();
+
+        adapter = new DriverOrdersAdapter(ordersList, fragmentManager);
+        orders.setLayoutManager(new LinearLayoutManager(requireContext()));
+        orders.setAdapter(adapter);
+
+        return view;
+    }
+
+    private void getOrders() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        String currentUserId = currentUser.getUid();
+        ordersList = new ArrayList<>();
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users").child(currentUserId).child("ordersId");
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot orderSnapshot : snapshot.getChildren()) {
+                    String orderId = orderSnapshot.getKey();
+                    ordersList.add(orderId);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
     }
 }
